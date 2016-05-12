a8b5dc08113bbc41a2546e6fd9fa1e736a1405af
Martin Pugh <martin@pughspace.com>
2016-05-11 21:48:23 -0400

    Multiple Updates
    1. Added Parameter sets
    2. Show-HDSize
    3. Simplified OS check
    4. Added support for multiple computers and partition selection.

--- Expand-Partition.ps1
+++ Expand-Partition.ps1
@@ -6,10 +6,10 @@ function Expand-Partition {
         Simple tool to resize partition on Windows Server 2012+
 
         User is prompted with a list of partitions that can be selected with Out-GridView to expand
-    .PARAMETER Disk
+    .PARAMETER PSComputerName
         Partition object from Get-Partition that can be passed via the pipeline
-    .PARAMETER ComputerName
-        ComputerName to expand a partition that has already been extended on in VMWare or SAN
+    .PARAMETER Name
+        ComputerName to expand a partition that has already been extended on in VMWare or SAN. Can accept pipeline input from Get-ADComputer
     .PARAMETER Credential
         Accepts a PSCreditial object passed to a remote computer via -Credential or creating a new CimSession using the credential
     .EXAMPLE
@@ -20,56 +20,106 @@ function Expand-Partition {
         Changelog:
             1.0         Initial Release
             1.1         Added Credential Parameter and Pester tests
+            1.2         MLP - Added Parameter sets, Show-HDSize, simplified OS check, added support for multiple computers and partition
+                        selection.
     #>
-    [CmdletBinding()]    
+    [CmdletBinding(DefaultParameterSetName="cli")]    
     param(
-        [parameter(ValueFromPipeline)]
-        [CimInstance]$InputObject,        
-        [string]$ComputerName="localhost",
+        [parameter(ValueFromPipeline,ParameterSetName="getp")]
+        [CimInstance]$PSComputerName,
+        
+        [Parameter(ValueFromPipelineByPropertyName,ParameterSetName="cli")]
+        [Alias("ComputerName")]    
+        [string[]]$Name="localhost",
+
         [pscredential]$Credential
     )
-    if($InputObject.PSComputerName){
-        $ComputerName = $InputObject.PSComputerName
-    }
-    
-    if($Credential){
-        if((Get-WmiObject -ComputerName $ComputerName -Credential $Credential -Class Win32_OperatingSystem | Select -ExpandProperty Caption) -like "*2008*" -or (Get-WmiObject -ComputerName $ComputerName -Credential $Credential -Class Win32_OperatingSystem | Select -ExpandProperty Caption) -like "*2003*"){
-            Write-Error "This is only supported on Windows Server 2012+"
-            return
-        }        
-        $CimSession = New-CimSession -Credential $Credential -ComputerName $ComputerName
-    }
-    else{
-        if((Get-WmiObject -ComputerName $ComputerName -Class Win32_OperatingSystem | Select -ExpandProperty Caption) -like "*2008*" -or (Get-WmiObject -ComputerName $ComputerName -Class Win32_OperatingSystem | Select -ExpandProperty Caption) -like "*2003*"){
-            Write-Error "This is only supported on Windows Server 2012+"
-            return
+
+    Begin {
+        Function Show-HDSize {
+            #Return disk size converted to closest size
+            Param (
+                [int64]$Size
+            )
+
+            If ($Size -gt 1125899906842624)
+            {
+                $Result = "{0:N2} PB" -f $Size / 1PB
+            }
+            ElseIf ($Size -gt 1099511627776)
+            {
+                $Result = "{0:N2} TB" -f $Size / 1TB
+            }
+            ElseIf ($Size -gt 1073741824)
+            {
+                $Result = "{0:N2} GB" -f $Size / 1GB
+            }
+            Else
+            {
+                $Result = "{0:N2} MB" -f $Size / 1MB
+            }
+            Return $Result
         }
-        $CimSession = $ComputerName        
+        $ComputerNames = @()
     }
-    
-    if($InputObject){
-        $Partitions = @($InputObject)
-    }
-    else{
-        $Partitions = Get-Partition -CimSession $CimSession | ? Type -ne "Reserved" | Out-GridView -PassThru
-    }
-    if($Partitions){
-        foreach($Partition in $Partitions){
-            Update-HostStorageCache -CimSession $CimSession
-            $MaxSize =  ($Partition | Get-PartitionSupportedSize -CimSession $CimSession).SizeMax
-            if($MaxSize -ne $Partition.Size){
-                $Partition | Resize-Partition -CimSession $CimSession -Size $MaxSize -ErrorAction Stop
+
+    Process {
+        If ($PsCmdlet.ParameterSetName -eq "getp")
+        {
+            If ($PSComputerName)
+            {
+                $ComputerNames += $PSComputerName
             }
-            else{
-                Write-Error "Drive is unable to be expanded because it is already at the full size. Verify the drive has been expanded in VMWare or the SAN" -ErrorAction Stop
+            Else
+            {
+                $ComputerNames += "localhost"
             }
-            [PSCustomObject]@{
-                "DriveLetter"=$Partition.DriveLetter
-                "PartitionNumber"=$Partition.PartitionNumber
-                "DiskNumber"=$Partition.DiskNumber
-                "PreviousSize"="$('{0:N2}' -f(($Partition | Select -ExpandProperty Size)/1024/1024/1024)) GB"
-                "CurrentSize"="$('{0:N2}' -f(($Partition | Get-Partition -CimSession $CimSession | Select -ExpandProperty Size)/1024/1024/1024)) GB"
-                "ComputerName"=$CimSession
+        }
+    }
+
+    End {
+        If ($PsCmdlet.ParameterSetName -eq "getp")
+        {
+            $Name = $ComputerNames | Select -Unique
+        }
+
+        ForEach ($Computer in $Name)
+        {
+            $CimSplat = @{
+                ComputerName = $Computer
+            }
+            If ($Credential)
+            {
+                $CimSplat.Add("Credential",$Credential)
+            }
+
+            If ((Get-CimInstance -ClassName Win32_OperatingSystem @CimSplat | Select -ExpandProperty Caption) -match "200[038]")
+            {
+                Write-Error "This is only supported on Windows Server 2012+"
+                Return
+            }
+            $CimSession = New-CimSession @CimSplat
+   
+            $Partitions = Get-Partition -CimSession $CimSession | ? Type -ne "Reserved" | Out-GridView -OutputMode Multiple -Title "Select the partitions you wish to expand"
+            if($Partitions){
+                foreach($Partition in $Partitions){
+                    Update-HostStorageCache -CimSession $CimSession
+                    $MaxSize =  ($Partition | Get-PartitionSupportedSize -CimSession $CimSession).SizeMax
+                    if($MaxSize -ne $Partition.Size){
+                        $Partition | Resize-Partition -CimSession $CimSession -Size $MaxSize -ErrorAction Stop
+                    }
+                    else{
+                        Write-Error "Drive is unable to be expanded because it is already at the full size. Verify the drive has been expanded in VMWare or the SAN" -ErrorAction Stop
+                    }
+                    [PSCustomObject]@{
+                        "DriveLetter"=$Partition.DriveLetter
+                        "PartitionNumber"=$Partition.PartitionNumber
+                        "DiskNumber"=$Partition.DiskNumber
+                        "PreviousSize"=Show-HDSize -Size $Partition.Size     #"$('{0:N2}' -f(($Partition | Select -ExpandProperty Size)/1024/1024/1024)) GB"
+                        "CurrentSize"=Show-HDSize -Size ($Partition | Get-Partition -CimSession $CimSession).Size    #"$('{0:N2}' -f(($Partition | Get-Partition -CimSession $CimSession | Select -ExpandProperty Size)/1024/1024/1024)) GB"
+                        "ComputerName"=$Computer
+                    }
+                }
             }
         }
     }
